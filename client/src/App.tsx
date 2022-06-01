f73377ce3fab0a2ea8edec4df6b902b230bc6e05
Iain McKenzie <iain.mckenziestrath@gmail.com>
2022-06-01 12:38:59 +0100

    add product page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import NavBar from './components/nav/NavBar';
 import Home from "./components/home/Home"
 import Login from './components/login/Login';
 import Profile from './components/profile/Profile';
+import ProductPage from './components/product/ProductPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Login />} />
         <Route path="/:id" element={<Profile />} />
+        <Route path="/products/:id" element={<ProductPage />} />
       </Routes>
     </div>
   );
---
+++ client/src/components/product/ProductPage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button, Card, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+
+export default function ProductPage () {
+    return (
+        <Container className="mt-4">
+            <Row>
+                <Col>
+                    <Container>
+                        <Card style={{width: "100%"}}>
+                            <Card.Img src="BCD.jpg" />
+                        </Card>
+                    </Container>
+                </Col>
+                <Col>
+                    <h2>Product Name</h2>
+                    <h4>£20.99</h4>
+                    <p className="text-start fs-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid quo iusto, voluptates architecto commodi quibusdam. Dolorem, itaque. Labore quisquam et consequatur sint, nobis expedita? Quaerat est numquam saepe maiores facilis sequi et consequuntur rerum assumenda vero quisquam perferendis adipisci provident ea nemo neque ipsam nulla, consequatur, voluptatem quis. At perspiciatis consectetur aliquam itaque corrupti ullam natus facere officia! Alias hic minima sint magnam quas veniam eius fugiat illum quis? Omnis corrupti provident, ipsa, corporis quidem beatae, laudantium eveniet numquam similique quisquam odio minus. Esse, voluptatem. Dicta placeat ipsum tempora cum. Quidem voluptas iure soluta et itaque numquam distinctio recusandae esse.</p>
+
+                    <Button variant="success" type="submit">Add to Cart</Button>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
